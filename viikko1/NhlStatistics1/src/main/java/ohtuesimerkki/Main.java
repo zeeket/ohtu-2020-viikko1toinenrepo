1768208dcfa4819b7a2f8ff22d5a804a9396bee0
zeeket <3qvm@tutanota.com>
2020-11-02 23:36:54 +0200

    15

--- viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
+++ viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Main.java
@@ -2,7 +2,8 @@ package ohtuesimerkki;
 
 public class Main {
     public static void main(String[] args) {
-        Statistics stats = new Statistics();
+        PlayerReader r = new PlayerReader("https://nhlstatisticsforohtu.herokuapp.com/players.txt");
+        Statistics stats = new Statistics(r);
           
         System.out.println("Philadelphia Flyers");
         for (Player player : stats.team("PHI") ) {
--- viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
+++ viikko1/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
@@ -9,8 +9,7 @@ public class Statistics {
 
     private List<Player> players;
 
-    public Statistics() {
-        PlayerReader reader = new PlayerReader("https://nhlstatisticsforohtu.herokuapp.com/players.txt");
+    public Statistics(Reader reader) {
         players = reader.getPlayers();       
     }
 
